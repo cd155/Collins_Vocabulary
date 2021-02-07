01c409574a49814758e7967f796a30b12e34fbe3
Don <cd155@163.com>
2021-02-07 20:50:18 +0800

    add comment for how to use the loader

--- loader.py
+++ loader.py
@@ -1,9 +1,15 @@
+# Change file path to either five_star or four_star to load 
+# related vocabulary. One_star and Two_star have different
+# format, and it already cleared.
 file1 = open('Vocabulary_Lists/three_star.txt', 'r') 
 lines = file1.readlines() 
 
-# example
+# Data format example
 # 1.	abandon[ә'bændәn]vt.丢弃,放弃,抛弃
 noun_list = []
+
+# Change this to n, a, adj, adv, vt, vi, num, ad, and prep to
+# get related type of vocabulary you need.
 type_vocabulary = 'n'
 
 for line in lines: 
@@ -17,11 +23,12 @@ for line in lines:
         if type_vocabulary in rear_part:
             noun_list.append(front_bracket_split[0].strip())
 
+# Use random to select top 50 word you want to write down
 import random
 top_50_random = random.sample(noun_list, 50)
 
 print("Noun typle:")
 
 for word in top_50_random:
-    # print(word)
-    print( '{{pause}}{{pause}}{{pause}}{{pause}}' + word)
+    # print(word) # print the word directly
+    print( '{{pause}}{{pause}}{{pause}}{{pause}}' + word) # print word with pause notation
